67429cf5eda1a22424332cd497d4a657c91fcaa1
hjrgrn <187955624+hjrgrn@users.noreply.github.com>
2026-01-08 09:38:21 +0100

    worker: Add Worker.runMap helper

--- cmd/worker/types/worker.go
+++ cmd/worker/types/worker.go
@@ -48,61 +48,7 @@ func (w *Worker) Work(mapf func(string, string) utils.ByKey,
 		if state == Done {
 			break
 		} else if state == WorkingOnMap {
-			go func() {
-				reply, ok := CallGetMapTask()
-				for !ok {
-					// TODO: wait a certain amount of time before retrying, retry only a certain
-					// amount of time
-					reply, ok = CallGetMapTask()
-				}
-
-				if reply.MapIsCompleted {
-					w.mutex.Lock()
-					w.state = WorkingOnReduce
-					w.mutex.Unlock()
-				} else {
-					// TODO: we are working on the same filesystem at the moment
-					filename := string(reply.Path)
-					file, err := os.Open(filename)
-					if err != nil {
-						log.Fatalf("cannot open %v", filename)
-					}
-					content, err := io.ReadAll(file)
-					if err != nil {
-						log.Fatalf("cannot read %v", filename)
-					}
-					file.Close()
-					kva := mapf(filename, string(content))
-
-					// save files
-					buckets := make(map[int]Bucket, reply.Buckets)
-					for i := range reply.Buckets {
-						buckets[i] = NewBucket(i)
-					}
-					for _, kv := range kva {
-						// TODO: error handling
-						index, _ := strconv.Atoi(kv.Key)
-						hash := index % reply.Buckets
-						writer := buckets[hash].Writer
-						// TODO: error handling
-						writer.Write([]string{kv.Key, kv.Value})
-						writer.Flush()
-					}
-
-					// TODO: Setup RPCs for other clients
-
-					l, e := net.Listen("tcp", ":0")
-					if e != nil {
-						log.Fatal("listen error:", e)
-					}
-					addr := l.Addr().(*net.TCPAddr)
-					addr_port := addr.AddrPort()
-					CallMapCompleted(reply.Path, addr_port)
-
-					// TODO: delete this
-					l.Close()
-				}
-			}()
+			go w.runMap(mapf)
 		} else {
 			// TODO:
 			w.mutex.Lock()
@@ -112,4 +58,62 @@ func (w *Worker) Work(mapf func(string, string) utils.ByKey,
 		// TODO: close buckets files
 	}
 
+}
+
+// Runs the map task. It's supposed to be run in a separate routine.
+func (w *Worker) runMap(mapf func(string, string) utils.ByKey,
+) {
+	reply, ok := CallGetMapTask()
+	for !ok {
+		// TODO: wait a certain amount of time before retrying, retry only a certain
+		// amount of time
+		reply, ok = CallGetMapTask()
+	}
+
+	if reply.MapIsCompleted {
+		w.mutex.Lock()
+		w.state = WorkingOnReduce
+		w.mutex.Unlock()
+	} else {
+		// TODO: we are working on the same filesystem at the moment
+		filename := string(reply.Path)
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Fatalf("cannot open %v", filename)
+		}
+		content, err := io.ReadAll(file)
+		if err != nil {
+			log.Fatalf("cannot read %v", filename)
+		}
+		file.Close()
+		kva := mapf(filename, string(content))
+
+		// save files
+		buckets := make(map[int]Bucket, reply.Buckets)
+		for i := range reply.Buckets {
+			buckets[i] = NewBucket(i)
+		}
+		for _, kv := range kva {
+			// TODO: error handling
+			index, _ := strconv.Atoi(kv.Key)
+			hash := index % reply.Buckets
+			writer := buckets[hash].Writer
+			// TODO: error handling
+			writer.Write([]string{kv.Key, kv.Value})
+			writer.Flush()
+		}
+
+		// TODO: Setup RPCs for other clients
+
+		l, e := net.Listen("tcp", ":0")
+		if e != nil {
+			log.Fatal("listen error:", e)
+		}
+		addr := l.Addr().(*net.TCPAddr)
+		addr_port := addr.AddrPort()
+		CallMapCompleted(reply.Path, addr_port)
+
+		// TODO: delete this
+		l.Close()
+	}
 }
