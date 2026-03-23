0f480bc22a7dafa6132cb81b96b47ccd5ee9761f
hjrgrn <187955624+hjrgrn@users.noreply.github.com>
2026-03-23 11:09:06 +0100

    worker: fix bug in NewBucket

--- cmd/worker/types/bucket.go
+++ cmd/worker/types/bucket.go
@@ -36,7 +36,7 @@ func NewBucket(id int) Bucket {
 	// TODO: Path, maybe use "/var/tmp"
 	rand.New(rand.NewSource(time.Now().UnixNano()))
 	// TODO: use path/filepath
-	path := "./instace/" + strconv.Itoa(os.Getpid()) + "-" + strconv.Itoa(id) + "-" + strconv.Itoa(rand.Intn(1000000))
+	path := "./instance/" + strconv.Itoa(os.Getpid()) + "-" + strconv.Itoa(id) + "-" + strconv.Itoa(rand.Intn(1000000))
 	file, err := os.Create(path)
 	writer := csv.NewWriter(file)
 	reader := csv.NewReader(file)
